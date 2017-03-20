e5e370d098601f642cc966c6b1636925ab3980c2
Deborah RECOURSE <drecours@e3r1p12.42.fr>
2017-03-20 11:43:37 +0100

    base de la base

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,32 @@
 /*   By: drecours <drecours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/13 11:26:29 by drecours          #+#    #+#             */
-/*   Updated: 2017/03/18 17:47:55 by drecours         ###   ########.fr       */
+/*   Updated: 2017/03/20 11:43:18 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "ft_printf.h"
+
+void	ft_index_max(t_env *env)
+{
+	ft_putstr((char const *) env->buffer);
+	ft_bzero(&env->buffer, BUFF_SIZE);
+	env->index = 0;
+}
+
+void	ft_putinit(t_env *env, char *addit)
+{
+	int		i;
+
+	i = -1;
+	while (addit[++i])
+	{
+		env->buffer[env->index] = addit[i];
+		if (env->index++ >= BUFF_SIZE)
+			ft_index_max(env);
+	}
+}
 
 int		ft_printf(const char *format, ...)
 {
@@ -26,11 +46,15 @@ int		ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (env.str[++env.index])
 	{
-		
-		if (env.str[i] == '%')
-			ft_search;
+		env.buffer[0] = 'a';
+		ft_putinit(&env, "hello");
+		break;
+		/*if (env.str[i] == '%')
+			ft_search;*/
 	}
 	printf("%d", env.index);
+	env.index = 0;
+	printf("%s", env.buffer);
 	va_end(args);
 	return 0;
 }
