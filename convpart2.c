1420c9f80a8a58f5d9dc4f5e9886963f57429fc7
Deborah RECOURSE <drecours@e3r1p14.42.fr>
2017-04-01 13:51:54 +0200

    dernieres corrections sur %o

--- convpart2.c
+++ convpart2.c
@@ -6,7 +6,7 @@
 /*   By: drecours <drecours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/25 11:54:51 by drecours          #+#    #+#             */
-/*   Updated: 2017/03/30 17:44:18 by drecours         ###   ########.fr       */
+/*   Updated: 2017/04/01 13:51:24 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,25 +72,25 @@ static void			convoplus(t_env *env)
 	int		i;
 
 	i = ft_strlen(env->conv.nb) + env->conv.sharp;
-	while (i++ < env->conv.precision)
-		;
-	if (env->conv.precision == 0 && env->conv.nb[0] == '0' &&
-				env->conv.nb[1] == '\0')
-		i = env->conv.sharp + 1;
-	while (env->conv.champ-- >= i)
+	while (i < env->conv.precision)
+		i++;
+	if (env->conv.nb[0] == '0' && env->conv.nb[1] == '\0')
+		i = env->conv.sharp;
+	while (env->conv.champ > i)
 	{
 		if (env->conv.zero == 1)
 			ft_putinit(env, "0", -1);
 		else
 			ft_putinit(env, " ", -1);
+		env->conv.champ--;
 	}
-	i = ft_strlen(env->conv.nb) + env->conv.sharp;
-	while (i++ < env->conv.precision)
-		ft_putinit(env, "0", -1);
+	i = ft_strlen(env->conv.nb);
 	if (env->conv.sharp == 1)
 		ft_putinit(env, "0", -1);
-	if (!(env->conv.precision == 0 && env->conv.nb[0] == '0' &&
-				env->conv.nb[1] == '\0'))
+	while (i++ < env->conv.precision)
+		ft_putinit(env, "0", -1);
+	if (!( env->conv.nb[0] == '0' && env->conv.nb[1] == '\0'
+				&& env->conv.precision > -1))
 		ft_putinit(env, env->conv.nb, -1);
 }
 
@@ -106,9 +106,11 @@ void			convo(va_list args, t_env *env)
 			ft_putinit(env, "0", -1);
 		while (i++ < env->conv.precision)
 			ft_putinit(env, "0", -1);
-		if (!(env->conv.precision == 0 && env->conv.nb[0] == '0' && 
-					env->conv.nb[1] == '\0'))
+		if (!( env->conv.nb[0] == '0' && env->conv.nb[1] == '\0' 
+					&& env->conv.precision <= 0 && env->conv.champ != 0))
 			ft_putinit(env, env->conv.nb, -1);
+	if (env->conv.nb[0] == '0' && env->conv.nb[1] == '\0')
+		i--;
 		while (env->conv.champ-- >= i)
 			ft_putinit(env, " ", -1);
 	}
