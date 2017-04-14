f7371524ef17babf8c713d9d48ada74dec1a15f1
Deborah RECOURSE <drecours@e3r1p12.42.fr>
2017-04-14 13:11:14 +0200

    les flags etaient mal recuperergit add *git add *

--- get_data.c
+++ get_data.c
@@ -6,7 +6,7 @@
 /*   By: drecours <drecours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 18:10:46 by drecours          #+#    #+#             */
-/*   Updated: 2017/04/13 19:51:10 by drecours         ###   ########.fr       */
+/*   Updated: 2017/04/14 13:09:25 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,25 +31,26 @@ static void		get_features(t_env *env, const char *format, int i)
 static int		get_conv(t_env *env, int j, int i, const char *format)
 {
 		env->conv.conversion = j;
-	if ((j == 11 || j == 13 || j == 7 || j == 9 || j == 4 || j == 1)
-			&& format[env->indexstr + i - 1] == 'l')
+	if ((j == 1 || j == 4 || j == 5 || j == 7 || j == 9 || j == 10 || j == 11
+				|| j == 13) && format[env->indexstr + i - 1] == 'l')
 	{
-		if (format[env->indexstr + i - 2] == 'l')
-			env->l = 1;
-		else 
+		if (format[env->indexstr + i - 2] == 'l' && j != 1 && j != 13)
 			env->ll = 1;
+		else 
+			env->l = 1;
 	}
-	if ((j == 11 || j == 13 || j == 7 || j == 9 || j == 4 || j == 1)
+	if ((j == 4 || j == 5 || j == 7 || j == 9 || j == 10 || j == 11)
 			&& format[env->indexstr + i - 1] == 'h')
 	{
 		if (format[env->indexstr + i - 2] == 'h')
-			env->h = 1;
-		else 
 			env->hh = 1;
+		else 
+			env->h = 1;
 	}
-	if ((j == 5 || j == 4 || j == 3) && format[env->indexstr + i - 1] == 'j')
+	if ((j == 5 || j == 4 || j == 7 || j == 9 || j == 10 || j == 11)
+			&& format[env->indexstr + i - 1] == 'j')
 		env->j = 1;
-	if ((j == 11 || j == 13 || j == 7 || j == 9 || j == 4 || j == 1)
+	if ((j == 4 || j == 5 || j == 7 || j == 9 || j == 10 || j == 11)
 			&& format[env->indexstr + i - 1] == 'z')
 		env->z = 1;
 	env->conv.conversion = j;
