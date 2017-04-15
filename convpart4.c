9a80a4d2b4d9059e7b59db8c9c28df65d0a7e1fa
Deborah RECOURSE <drecours@e3r1p14.42.fr>
2017-04-15 14:27:11 +0200

    small errors are behind me, take care to what you write you little brat

--- convpart4.c
+++ convpart4.c
@@ -6,25 +6,18 @@
 /*   By: drecours <drecours@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/06 13:33:19 by drecours          #+#    #+#             */
-/*   Updated: 2017/04/14 15:11:43 by drecours         ###   ########.fr       */
+/*   Updated: 2017/04/15 14:24:41 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void		convxplus(t_env *env)
+static void		convxplus(t_env *env, int i)
 {
-	int	i;
-
-	i = ft_strlen(env->conv.nb) + ((env->conv.sharp == 1) ? 2 : 0);
-	if (env->conv.champ > 0 && env->conv.nb[0] == '0' && env->conv.precision == 0)
-		env->conv.champ++;
 	if (env->conv.sharp == 1 && env->conv.nb[0] != '0' && env->conv.zero == 1)
 		ft_putinit(env, (env->conv.conversion == 10) ? "0X" : "0x", -1);
 	while (i < env->conv.precision)
 		i++;
-	if (env->conv.nb[0] == '0')
-		i = env->conv.sharp + 1;
 	while (env->conv.champ > i)
 	{
 		if (env->conv.zero == 1)
@@ -38,7 +31,7 @@ static void		convxplus(t_env *env)
 		ft_putinit(env, (env->conv.conversion == 10) ? "0X" : "0x", -1);
 	while (i++ < env->conv.precision)
 		ft_putinit(env, "0", -1);
-	if (!( env->conv.nb[0] == '0' && env->conv.precision > -1))
+	if (!( env->conv.nb[0] == '0' && env->conv.precision == 0))
 		ft_putinit(env, env->conv.nb, -1);
 }
 
@@ -54,8 +47,7 @@ void	convx(va_list args, t_env *env)
 			ft_putinit(env, (env->conv.conversion == 10) ? "0X" : "0x", -1);
 		while (i++ < env->conv.precision)
 			ft_putinit(env, "0", -1);
-		if (!( env->conv.nb[0] == '0' && env->conv.precision <= 0
-					&& env->conv.champ != 0))
+		if (!( env->conv.nb[0] == '0' && env->conv.precision <= 0))
 			ft_putinit(env, env->conv.nb, -1);
 		if (env->conv.nb[0] == '0')
 			i--;
@@ -63,7 +55,7 @@ void	convx(va_list args, t_env *env)
 			ft_putinit(env, " ", -1);
 	}
 	else
-		convxplus(env);
+		convxplus(env, i);
 }
 
 void	convgx(va_list args, t_env *env)
