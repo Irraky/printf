3227429faa3ac29ce1c976957a0ec3910c2a0dc8
Deborah RECOURSE <drecours@e3r1p13.42.fr>
2017-04-22 17:41:31 +0200

    %C seems good but %S does nt work

--- convpart3.c
+++ convpart3.c
@@ -6,40 +6,52 @@
 /*   By: drecours <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:06:36 by drecours          #+#    #+#             */
-/*   Updated: 2017/04/19 18:23:08 by drecours         ###   ########.fr       */
+/*   Updated: 2017/04/22 17:04:12 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <string.h>
 
-void			convgc(va_list args, t_env *env)
+static void			trick(wchar_t args, t_env *env)
 {
-	if (va_arg(args, wchar_t) <= 0x7F)
-		ft_putinit(env, va_arg(args, char*), -1);
-	else if (va_arg(args, wchar_t) <= 0x7FF)
+	if ((unsigned)args < 0x80)
+		env->wchar[0] = args;
+	else if ((unsigned)args <= 0x800)
 	{
-		env->wchar[0] = (va_arg(args, wchar_t) >> 6) + 0xC0;
-		env->wchar[1] = (va_arg(args, wchar_t) & 0x3F) + 0x80;
+		env->wchar[0] = ((args >> 6) + 0xC0);
+		env->wchar[1] = ((args & 0x3F) + 0x80);
 	}
-	else if (va_arg(args, wchar_t) <= 0xFFFF)
+	else if ((unsigned)args <= 0xd800 || (unsigned)args - 0xE000 < 0x2000)
 	{
-		env->wchar[0] = (va_arg(args, wchar_t) >> 12) + 0xE0;
-		env->wchar[1] = ((va_arg(args, wchar_t) >> 6) & 0x3F)+ 0x80;
-		env->wchar[2] = (va_arg(args, wchar_t) & 0x3F) + 0x80;
+		env->wchar[0] = ((args >> 12) + 0xE0);
+		env->wchar[1] = (((args >> 6) & 0x3F) + 0x80);
+		env->wchar[2] = ((args & 0x3F) + 0x80);
 	}
-	else if (va_arg(args, wchar_t) <= 0x10FFFF)
+	else if ((unsigned)args - 0x10000 < 0x100000)
 	{
-		env->wchar[0] = (va_arg(args, wchar_t) >> 18) + 0xF0;
-		env->wchar[1] = ((va_arg(args, wchar_t) >> 12) & 0x3F) + 0x80;
-		env->wchar[2] = ((va_arg(args, wchar_t) >> 6) & 0x3F) + 0x80;
-		env->wchar[3] = (va_arg(args, wchar_t) & 0x3F) + 0x80;
+		env->wchar[0] = ((args >> 18) + 0xF0);
+		env->wchar[1] = (((args >> 12) & 0x3F) + 0x80);
+		env->wchar[2] = (((args >> 6) & 0x3F) + 0x80);
+		env->wchar[3] = ((args & 0x3F) + 0x80);
 	}
-	if (va_arg(args, wchar_t) <= 0x10FFFF && va_arg(args, wchar_t) > 0x7f)
+	if (env->wchar[0] != '\0')
 		ft_putinit(env, env->wchar, -1);
+}
+
+void			convgc(va_list arg, t_env *env)
+{
+	trick(va_arg(arg, wchar_t), env);
 }
 
 void			convgs(va_list args, t_env *env)
 {
-	(void)args;
-	(void)env;
+	wchar_t	*s;
+
+	s = va_arg(args, wchar_t *);
+	while (s && *s)
+	{
+		trick(*s, env);
+		s++;
+	}
 }
--- main.c
+++ main.c
@@ -6,21 +6,24 @@
 /*   By: drecours <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 13:56:22 by drecours          #+#    #+#             */
-/*   Updated: 2017/04/19 17:59:40 by drecours         ###   ########.fr       */
+/*   Updated: 2017/04/22 17:10:19 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
-#include <locale.h>
+#include<stdlib.h>
 
 int		main(void)
 {
 	int	i;
 
 	i= 1;
-	setlocale(LC_ALL, "");
-	ft_printf("ft_printf : |%C|\n", L'Ȣ');
-	ft_printf("ft_printf : |%C|", L'鯶');
+	//ft_printf("ft_printf : |%S|\n\n", "ȢȢii");
+	printf("printf 0: |%C|\n", 0);
+	ft_printf("ft_printf 0: |%C|\n", 0);
+	ft_printf("ft_printf : |%C|\n", L'1');
+	ft_printf("ft_printf : |%C|\n", L'a');
+
 	return 0;
 }
