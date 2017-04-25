7170a79cde76db4f481dd2bcf49d56dbac0fa5b0
Deborah RECOURSE <drecours@e3r2p23.42.fr>
2017-04-25 16:50:51 +0200

    look at setlocal

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: drecours <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 13:56:22 by drecours          #+#    #+#             */
-/*   Updated: 2017/04/24 15:21:02 by drecours         ###   ########.fr       */
+/*   Updated: 2017/04/25 16:45:15 by drecours         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include <wchar.h>
+#include <locale.h>
 
 int		main(void)
 {
 	int	i;
 
 	i = 1;
-	printf("|%*.*x|\n", 7, 2, 34545);
-	 ft_printf("|%*.*x|", 7,2, 34545);
-	//ft_printf("ft_printf : |%C|\n", L'1');
+	//printf("|valeur printf: %d|\n", printf("%S", setlocale(LC_ALL, "米")));
+	printf("|valeur ft_printf: %d|\n", ft_printf("%S", L"Á±≥"));
+	//ft_printf("{%}");
+	// printf("|%*.*d|\n", 7, 2, );
+	 //ft_printf("|%*.*d|", 7);
+//	ft_printf("ft_printf : |%C|\n", L'1');
 	//ft_printf("ft_printf : |%C|\n", L'a');
 
 	return 0;
